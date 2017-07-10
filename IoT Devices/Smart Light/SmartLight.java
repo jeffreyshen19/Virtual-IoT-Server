3f56d55dfb93285b0549548626379372d6b0c1f2
jeffreyshen19 <jshen@andover.edu>
2017-07-10 12:58:07 -0700

    socket code

--- IoT Devices/Smart Light/SmartLight.java
+++ IoT Devices/Smart Light/SmartLight.java
@@ -32,51 +32,44 @@ public class SmartLight{
   }
 
   public static void main(String[] args) {
-    Gpio led = new Gpio(4);
-    Aio lightSensor = new Aio(0);
-
-    led.dir(Dir.DIR_OUT);
-
-    int on = 0;
-    long lightValue = 0;
-    String command = "";
-
-    Socket clientSocket = null;
-    BufferedReader in = null;
-
     try{
-      clientSocket = new Socket(args[0], Integer.parseInt(args[1]));
-    }
-    catch(Exception e){}
+      Gpio led = new Gpio(4);
+      Aio lightSensor = new Aio(0);
 
-    System.out.println("\033[1m\033[32mSuccessfully connected to server\033[0m");
+      led.dir(Dir.DIR_OUT);
 
-    try{
-      in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-    }
-    catch(Exception e){}
+      int on = 0;
+      long lightValue = 0;
+      String command = "";
 
-    while(true){
-      lightValue = lightSensor.read();
-      try{
+      Socket clientSocket = new Socket(args[0], Integer.parseInt(args[1]));
+      BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+      System.out.println("\033[1m\033[32mSuccessfully connected to server\033[0m");
+
+      while(true){
+        lightValue = lightSensor.read();
+
         command = in.readLine().trim().toLowerCase();
+        
+        System.out.println(command);
+
+        if(command.equals("off")) on = 1;
+        else if(command.equals("on")) on = -1;
+        else if(command.equals("reset")) on = 0;
+
+        if(lightValue < 200 && on != -1){
+          led.write(1);
+        }
+        else if(on != 1){
+          led.write(0);
+        }
+
+        wait1Msec(10);
       }
-      catch(Exception e){}
+    }
+    catch(Exception e){
 
-      System.out.println(command);
-
-      if(command.equals("off")) on = 1;
-      else if(command.equals("on")) on = -1;
-      else if(command.equals("reset")) on = 0;
-
-      if(lightValue < 200 && on != -1){
-        led.write(1);
-      }
-      else if(on != 1){
-        led.write(0);
-      }
-
-      wait1Msec(10);
     }
   }
 }
