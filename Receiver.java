440ea3c0426a13a6cd6eb105fa5fbe52034ca679
hpz1213 <william.pz.he@gmail.com>
2017-07-10 10:27:24 -0700

    Add files via upload

---
+++ Receiver.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import javax.net.ssl.SSLSocket;
+import java.net.UnknownHostException;
+
+public class Receiver{
+
+  public String connect (String server, String port){
+    String message;
+    SSLSocket sslSocket = null;
+    SSLClientSocket mSSLClientSocket = new SSLClientSocket(server, port);
+    if(mSSLClientSocket.checkAndAddCertificates()) {
+      sslSocket = mSSLClientSocket.getSSLSocket();
+    }
+    else {
+      return;
+    }
+
+    try {
+      BufferedReader br = new BufferedReader(new InputStreamReader(sslSocket.getInputStream()));
+      PrintWriter pw = new PrintWriter(sslSocket.getOutputStream());
+      pw.println("Initiating connection from the client");
+      System.out.println("\033[1m\033[32mSuccessfully connected to secure server\033[0m");
+      pw.flush();
+      br.readLine();
+
+      while(true){
+        message = br.readLine().trim();
+        Thread.sleep(1000);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    return message;
+  }
+}
