8266a1d66c1069d36051970cf14b754876ad29ba
jeffreyshen19 <jshen@andover.edu>
2017-07-10 13:16:39 -0700

    added virtual service

---
+++ VirtualService.java
@@ -0,0 +1,10 @@
+/*
+  VirtualService.java
+  Main class holding all the code for the virtual service. Compile and run THIS file.
+*/
+
+public class VirtualService{
+  public static void main(String[] args) {
+    
+  }
+}
