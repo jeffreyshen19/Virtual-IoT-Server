e39b9f6a7be01d22dfc774d53a5836e7b6115817
mano254 <alok.elashoff@gmail.com>
2017-07-10 11:51:51 -0700

    Methods

--- IoT Devices/Smart Door Lock/SmartDoorLock.java
+++ IoT Devices/Smart Door Lock/SmartDoorLock.java
@@ -19,22 +19,30 @@ public class SmartDoorLock {
     Gpio button = new Gpio(3);
     Aio light = new Aio(3);
     Pwm servo = new Pwm(6);
-    //servo.write(0f);
+    unlock();
+    lock();
+
+  }
+
+  public static void unlock() {
     servo.enable(true);
     servo.period_ms(20);
     servo.pulsewidth_us(500);
     try {
-      TimeUnit.SECONDS.sleep(3);
-    }catch (InterruptedException e) {
-    }
-    //servo.write(1f);
-    servo.pulsewidth_us(2500);
-    try {
-      TimeUnit.SECONDS.sleep(3);
+      TimeUnit.SECONDS.sleep(1);
     }catch (InterruptedException e) {
     }
     servo.enable(false);
+  }
 
-
+  public static void lock() {
+    servo.enable(true);
+    servo.period_ms(20);
+    servo.pulsewidth_us(2500);
+    try {
+      TimeUnit.SECONDS.sleep(1);
+    }catch (InterruptedException e) {
+    }
+    servo.enable(false);
   }
 }
