466df6daea11c77b8edbf7061d435bf4089f40a4
hpz1213 <william.pz.he@gmail.com>
2017-07-11 10:49:09 -0700

    Update Plugin.java

--- Plugin.java
+++ Plugin.java
@@ -1,6 +1,6 @@
 /*
   Plugin.java
-  Reads plug-in's in the form of text files and perform manipulation to information obtained. 
+  Reads plug-in's in the form of .txt files and perform manipulation to information obtained. 
   Has capability of updating plug-in's. 
 */
 
