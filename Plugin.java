fcae470d93445669ac74c67faa086cddadc261e2
hpz1213 <william.pz.he@gmail.com>
2017-07-11 09:19:00 -0700

    deletes all spaces in text file when reading

--- Plugin.java
+++ Plugin.java
@@ -2,29 +2,36 @@ import java.io.*;
 import java.util.*;
 
 public class Plugin{
-  
+  public String name;
+
   /*
   testing method. This main method should never be called.
   */
   public static void main(String[] args) {
-    ArrayList<String> array = readtxt(args[0]);
-    
+    Plugin plugin1 = new Plugin ("door");
+    ArrayList<String> array = plugin1.readtxt(args[0]);
+
     for(int i = 0; i < array.size(); i++){
       System.out.println(array.get(i));
     }
   }
-  
+
+  public Plugin(String inName){
+    name = inName;
+  }
+
   /*
   reads the selected text file.
   fileLocation is the directory, like "/Users/PeizeHe/Desktop/test.txt"
   */
-  public static ArrayList<String> readtxt(String fileLocation){
+  public ArrayList<String> readtxt(String fileLocation){
     ArrayList<String> protocol = new ArrayList<String>();
     BufferedReader br = null;
     FileReader fr = null;
-    
+
     String inter = "";
-    
+    String element = "";
+
     try {
       fr = new FileReader(fileLocation);
       br = new BufferedReader(fr);
@@ -45,14 +52,23 @@ public class Plugin{
         ex.printStackTrace();
       }
     }
-    
+
+    for(int i = 0; i < protocol.size(); i++){
+      for(int j = 0; j < protocol.get(i).length(); j++){
+        if(protocol.get(i).substring(j, j + 1).equals(" ")){
+          element = "" + protocol.get(i).substring(0, j) + protocol.get(i).substring(j + 1);
+          protocol.set(i, element);
+        }
+      }
+    }
+
     for(int i = 0; i < protocol.size(); i++){
       inter = protocol.get(i);
       if(protocol.get(i).equals("")){
         protocol.remove(i);
       }
     }
-    
+
     return protocol;
   }
 }
