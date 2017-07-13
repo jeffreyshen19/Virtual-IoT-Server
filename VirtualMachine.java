cc9272f278f7e181946ccc7fd82dd84e48d1f8ed
jeffreyshen19 <jshen@andover.edu>
2017-07-13 14:47:57 -0700

    acceptor accepts ssl and tls

--- VirtualMachine.java
+++ VirtualMachine.java
@@ -6,12 +6,15 @@
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.*;
+import javax.net.ssl.SSLSocket;
 
 public class VirtualMachine extends Thread {
   private String filename, server;
   private int serverPort;
   private Socket socket;
+  private SSLSocket sslSocket;
   private IoTDevice device;
+  private boolean isUsingSSl = false;
 
   public VirtualMachine(Socket ss, String f, IoTDevice d){
     super();
@@ -23,12 +26,28 @@ public class VirtualMachine extends Thread {
     device = d;
   }
 
+  public VirtualMachine(SSLSocket ss, String f, IoTDevice d){
+    super();
+
+    filename = f;
+    server = d.getServerIP();
+    serverPort = d.getServerPort();
+    sslSocket = ss;
+    device = d;
+    isUsingSSl = true;
+
+    System.out.println("wassup");
+  }
+
   public void run(){
     Logger logger = new Logger("logs/" + filename);
     Receiver receiver = new Receiver(server, serverPort);
     logger.println("Connected to " + server + " on port " + serverPort);
 
-    Sender sender = new Sender(socket);
+    Sender sender;
+
+    if(isUsingSSl) sender = new Sender(sslSocket);
+    else sender = new Sender(socket);
 
 
     String message = "";
