b0d1cf5fd98dedcdf61bdaea150e3eb215619863
mano254 <alok.elashoff@gmail.com>
2017-07-20 14:02:02 -0700

    Non-Blocking

--- IoT Devices/Smart Door Lock/SmartDoorLock.java
+++ IoT Devices/Smart Door Lock/SmartDoorLock.java
@@ -7,6 +7,7 @@
 /*
   Usage Notes
   - Must type # to trigger password input
+  - Must type CHANGE PASSWORD on user side to change the password
   - Follow prompts when inputting password
 */
 
@@ -52,7 +53,7 @@ public class SmartDoorLock {
 
       //Setting up input
       sslSocket.setSoTimeout(1000);
-      String userInput = "" , serverResponse = "", passEnter = "";
+      String serverResponse = "", userInput = "";
       BufferedReader br = new BufferedReader(new InputStreamReader(sslSocket.getInputStream()));
       PrintWriter pw = new PrintWriter(sslSocket.getOutputStream());
       BufferedReader tag = new BufferedReader(new InputStreamReader(System.in));
@@ -91,7 +92,7 @@ public class SmartDoorLock {
 
           }
           if (tag.ready()) {
-            passEnter = tag.readLine().trim();
+            userInput = tag.readLine().trim();
           }
         }
         //Send packets of locked/unlocked to server
@@ -100,19 +101,26 @@ public class SmartDoorLock {
         pw.flush();
 
         //Checks the password entered by button pattern
-        if (passEnter.equals ("#")) {
+        if (userInput.equals ("#")) {
           if (inputPassword(button) == password) {
             unlock(servo);
             System.out.println("Succesfully unlocked.");
-            passEnter = "";
+            userInput = "";
           }
           else {
             lock(servo);
             System.out.println("Unsuccesful unlock. Please try again.");
-            passEnter = "";
+            userInput = "";
           }
         }
 
+        if(userInput.equals ("CHANGE PASSWORD")) {
+          System.out.println("Verify current password by following the prompts.");
+          if (inputPassword(button) == password) {
+            System.out.println("Now change the password by follwing the prompts.");
+            password = inputPassword(button);
+          }
+        }
         //Analyzes server's message
 
         if(serverResponse.equals("LOCK")) {
@@ -139,8 +147,9 @@ public class SmartDoorLock {
               try {
                 password = Integer.parseInt(serverResponse);
               }
-              catch (Exception e) {
+              catch (NumberFormatException nfe) {
                 System.out.println("Server inputted too long a password.");
+                nfe.printStackTrace();
               }
               System.out.println("Succesful password change. New password is " + password);
               pw.flush();
