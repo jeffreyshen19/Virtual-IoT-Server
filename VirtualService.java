3ea3a39d3b876f292b2d4893f4a8c0c7f15e1447
mano254 <alok.elashoff@gmail.com>
2017-07-31 11:01:03 -0700

    Update VirtualService.java

--- VirtualService.java
+++ VirtualService.java
@@ -1,6 +1,6 @@
 /*
   VirtualService.java
-  Main class holding all the code for the virtual service. Compile and run THIS file.
+  Main class handling all the code for the virtual service through threads. Compile and run THIS file.
 */
 
 import java.net.InetAddress;
@@ -12,22 +12,22 @@ public class VirtualService{
       System.out.println("\033[1mIP Address: " + InetAddress.getLocalHost().getHostAddress() + "\033[0m");
     }
     catch(Exception e){
-      e.printStackTrace();//printStackTrace error messge
+      e.printStackTrace();
     }
 
     ArrayList<VirtualMachine> machines = new ArrayList<VirtualMachine>();//initializing array list
-    Logger logger = new Logger("logs/" + args[0]);//logger construction with 0 arguments so far
+    Logger logger = new Logger("logs/" + args[0]);//logger constructor
 
     Acceptor acceptorTLS = new Acceptor(machines, 2000, false, logger);//Acceptor constructor
     acceptorTLS.start();//start method is called
     logger.println("Started TLS acceptor on port 2000");
 
     Acceptor acceptorSSL = new Acceptor(machines, 6000, true, logger);//AcceptorSSL constructor
-    acceptorSSL.start();//start method called agaain
+    acceptorSSL.start();//start method called again
     logger.println("Started SSL acceptor on port 6000");
 
-    PluginFileTransfer fileTransferer = new PluginFileTransfer(machines);
-    fileTransferer.start();
+    PluginFileTransfer fileTransferer = new PluginFileTransfer(machines);//PluginFileTransfer constructor
+    fileTransferer.start();//start method called again
     logger.println("Started Plugin Acceptor on port 9000");
   }
 }
