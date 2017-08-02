154406a8b1a4e537b40534868a2ca78a04d087b3
mano254 <alok.elashoff@gmail.com>
2017-08-02 12:50:14 -0700

    LightSensorPluginBug.java

---
+++ Plugins/LightSensorPluginBug.java
@@ -0,0 +1,25 @@
+/*
+  Runtime exception to test if Virtual Service Crashes
+*/
+public class LightSensorPluginBug extends IoTDevice{
+
+    //Overwrites constructor
+
+    public LightSensorPlugin(int sP, String ip){
+      super(sP, ip);
+    }
+
+    //Defines filter. Called when server inputs the given message
+
+    public String filterMessage(String message){
+
+      if (message.equals("Bug")) {
+        message = null;
+        if (message.equals("Bug")) {
+  
+        }
+      }
+      return message;
+    }
+  }
+}
