20617c2cc70645fd755e02d9a71144596f750812
mano254 <alok.elashoff@gmail.com>
2017-08-08 10:48:22 -0700

    Plugin is an arg

--- IoT Devices/Smart Light/SmartLight.java
+++ IoT Devices/Smart Light/SmartLight.java
@@ -52,10 +52,10 @@ public class SmartLight{
       BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
       PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 
-      out.println(args[2] + ":" + args[3] + "|LightSensorPlugin");
+      //IoT Device supplies server ip and port and the name of the plugin
       
-      //test bugged version
-      //out.println(args[2] + ":" + args[3] + "|LightSensorPluginBug");
+      out.println(args[2] + ":" + args[3] + "|" + args[4]);
+      
       System.out.println(args[2] + ":" + args[3] + "SmartLight");
 
 
